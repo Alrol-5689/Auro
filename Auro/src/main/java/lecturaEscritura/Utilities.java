7b986372e5658f0df3a83004d3a7e459ecd06d6d
Alex <alejandrocordobarodriguez@gmail.com>
2025-06-10 00:59:46 +0200

    [Utilities] creo pedirDuracion

--- Auro/src/main/java/lecturaEscritura/Utilities.java
+++ Auro/src/main/java/lecturaEscritura/Utilities.java
@@ -1,5 +1,6 @@
 package lecturaEscritura;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -49,12 +50,42 @@ public class Utilities {
             }
         }
     }
+    
+    public Duration pedirDuracion(Scanner sc, String mensaje) {
+        System.out.println(mensaje);
+        while (true) {
+            try {
+                int horas = pedirInt(sc, "Horas: ");
+                int minutos = pedirInt(sc, "Minutos: ");
+                if (horas < 0 || minutos < 0 || minutos >= 60) {
+                    throw new IllegalArgumentException("Valores inválidos (minutos entre 0 y 59, horas >= 0)");
+                }
+                return Duration.ofHours(horas).plusMinutes(minutos);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Duración no válida: " + e.getMessage());
+                System.out.println("Por favor, introdúcela de nuevo.\n");
+            }
+        }
+    }
 
     public int pedirInt(Scanner sc, String mensaje) {
         System.out.print(mensaje);
         while (true) {
             try {
                 int out = Integer.parseInt(sc.nextLine());
+                return out;
+            } catch (NumberFormatException e) {
+                System.err.println("Error. Introduce solo números. ");
+                System.out.print(mensaje);
+            }
+        }
+    }
+
+    public double pedirDouble(Scanner sc, String mensaje) {
+        System.out.print(mensaje);
+        while (true) {
+            try {
+                double out = Double.parseDouble(sc.nextLine().replace(",", "."));
                 return out;
             } catch (NumberFormatException e) {
                 System.err.println("Error. Introduce solo números. ");
